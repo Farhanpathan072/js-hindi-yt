39a97a1df02b81f45ba04c342d8916a77451807d
Farhanpathan072 <155763196+Farhanpathan072@users.noreply.github.com>
2024-08-26 13:33:21 +0000

    variables dive

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144384
+let accountEmail = "farhanpathan072@gmail.com"
+var accountPassword = "12345"
+accountCity = "Surat"
+let accountState;
+
+accountEmail = "pfpfathan@gmail.com"
+
+accountPassword = "212121"
+
+accountCity = "Mumbai"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail, accountPassword, accountCity,accountState]);
