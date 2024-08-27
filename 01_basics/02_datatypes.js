b1913b742bbd75a81f72c8372ded9500d933a05e
Farhanpathan072 <155763196+Farhanpathan072@users.noreply.github.com>
2024-08-27 07:07:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+console.log(  3 
+    + 4  // maintain code readability or this type of practice is not good..
+)
+// alert() we cant use alert because we are using Nodejs, not the browser...
+console.log("Farhan Pathan") 
+
+
+let name = "Farhan pathan"
+let age = 19
+let isloggedIn = false
+let state;
+// number => 2 to the power 53
+//bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof "farhan");
+console.log(typeof age);
+
+console.log(typeof isloggedIn);
+
+console.log(typeof null); // type => object
+console.log(typeof undefined); // type => undefined
