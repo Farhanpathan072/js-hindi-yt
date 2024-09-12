eba0dc9738871fb9f9976c081e13e2c0ec4f0ec2
Farhanpathan072 <155763196+Farhanpathan072@users.noreply.github.com>
2024-09-12 06:14:25 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,10 +4,47 @@ if(true){
     let a = 10
     const b = 20
      
-    console.log(`INNER:  ${a}`);
+    // console.log(`INNER:  ${a}`);
     
 }
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+  const username = `farhan`
+
+  function two(){
+       const website = `youtube`
+       console.log(username);
+  }
+    //  console.log(website);
+     two()
+}
+
+one()
+
+if(true){
+     const username = `farhan`
+     if(username === `farhan`){
+        const website = `google`
+        console.log(`${username}  ${website}`);
+        
+     }
+     // console.log(website);
+     
+}
+// console.log(username);
+// <------------------------ interesting ----------------------->
+
+console.log(addone(5))           // execute no matter if the call is comes first here..
+function addone(num){            // Simple function defintion...
+     return num + 1
+}
+
+addTwo(5)                  // throws error because accessing addtTwo variable before its declaration... 
+const addTwo = function(num){   // when variable store the function it is called an expression.
+     return num + 2
+}
+
 
