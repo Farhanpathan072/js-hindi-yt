c601f2dbdbcf6a75bd13d792d9878be329fcf8d6
Farhanpathan072 <155763196+Farhanpathan072@users.noreply.github.com>
2024-09-12 06:58:31 +0000

    IIFE in Js

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expresions (IIFE)
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+    
+})();                    // note: use semicolon for ending the scope context it is very important... if you dont use ;  
+                            // then it will throw error
+
+( (name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+    
+})(`farhan`)
