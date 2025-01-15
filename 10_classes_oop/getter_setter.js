b72057915a2ea9d1fa458e46c847818188dbb04f
Farhanpathan072 <155763196+Farhanpathan072@users.noreply.github.com>
2025-01-15 13:16:53 +0000

    getter and setter with class level

---
+++ 10_classes_oop/getter_setter.js
@@ -0,0 +1,26 @@
+class User {
+    constructor(email, password){
+        this.email = email
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+         this._email = value
+    }
+    
+    get password(){
+        return `${this._password}farhan`
+    }
+
+    set password(value){
+        this._password = value
+    }
+}
+
+const farhan = new User("Farhan@meta.com", "xyz123")
+console.log(farhan.password);
+console.log(farhan.email);
